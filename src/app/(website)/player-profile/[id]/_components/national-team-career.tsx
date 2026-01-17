678ce82e8d46a23ea1aecca4264d571c88c9c711
Kongkon-79 <kongkonjowarder79@gmail.com>
2026-01-17 13:58:54 +0600

    problem fix up

--- src/app/(website)/player-profile/[id]/_components/national-team-career.tsx
+++ src/app/(website)/player-profile/[id]/_components/national-team-career.tsx
@@ -39,7 +39,7 @@ const NationalTeam = ({ id }: { id: string }) => {
 
     const personalInfo = data?.data?.national;
     return (
-        <div className='py-6'>
+        <div className='pb-6'>
             <div className="container  bg-white rounded-[16px] p-6 shadow-[0px_4px_24px_0px_#00000014
 ]">
                 <h3 className="text-2xl md:text-3xl lg:text-4xl text-primary font-normal leading-[120%] pb-5 md:pb-6">National Team Career</h3>
--- src/app/(website)/player-profile/[id]/page.tsx
+++ src/app/(website)/player-profile/[id]/page.tsx
@@ -16,7 +16,7 @@ const PlayerProfilePage = ({ params }: PlayerProfilePageProps) => {
   return (
     <div className="h-min-screen bg-white bg-[linear-gradient(105.34deg,_rgba(177,174,255,0.24)_9.41%,_rgba(255,255,255,0.24)_49.41%,_rgba(176,167,255,0.24)_100.83%)]">
       <PlayerInfo id={params?.id} />
-      <div className='container grid grid-cols-1 md:grid-cols-5 gap-6 md:px-0 pb-10'>
+      <div className='container grid grid-cols-1 md:grid-cols-5 gap-6 md:px-0 pb-6'>
         <div className="md:col-span-3">
           <PlayerRating />
         </div>
